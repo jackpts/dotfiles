8b8f2e47402d7c2b3e339878432d247e27ff8bcf
Jack Pts <jackpts1981@gmail.com>
2024-11-29 17:39:25 +0300

    [nvim] [angular] replace ngswitcher plugin w/ quick-switcher

--- .config/nvim/lua/plugins/angular.lua
+++ .config/nvim/lua/plugins/angular.lua
@@ -3,12 +3,56 @@ local map = vim.keymap.set
 return {
   -- Angular component switcher
   {
-    "softoika/ngswitcher.vim",
+    "Everduin94/nvim-quick-switcher",
     init = function()
-      map("n", "<leader>at", ":NgSwitchHTML<cr>", { desc = "Goto Template" })
-      map("n", "<leader>ac", ":NgSwitchTS<cr>", { desc = "Goto Component" })
-      map("n", "<leader>as", ":NgSwitchCSS<cr>", { desc = "Goto CSS" })
-      map("n", "<leader>ae", ":NgSwitchSpec<cr>", { desc = "Goto Test" })
+      map(
+        "n",
+        "<leader>ac",
+        "<cmd>:lua require('nvim-quick-switcher').find('.component.ts')<CR>",
+        { noremap = true, silent = true, desc = "Goto Class" }
+      )
+      map(
+        "n",
+        "<leader>at",
+        "<cmd>:lua require('nvim-quick-switcher').find('.component.html')<CR>",
+        { noremap = true, silent = true, desc = "Goto Template" }
+      )
+      map(
+        "n",
+        "<leader>as",
+        "<cmd>:lua require('nvim-quick-switcher').find('.+css|.+scss|.+sass|.+less|.+style', { regex = true, prefix='full' })<CR>",
+        { noremap = true, silent = true, desc = "Goto Style" }
+      )
+      map(
+        "n",
+        "<leader>ae",
+        "<cmd>:lua require('nvim-quick-switcher').find('.+spec|.+test|.+e2e-spec.ts', { regex = true, prefix='full' })<CR>",
+        { noremap = true, silent = true, desc = "Goto tEst" }
+      )
+      map(
+        "n",
+        "<leader>am",
+        "<cmd>:lua require('nvim-quick-switcher').find('.module.ts')<CR>",
+        { noremap = true, silent = true, desc = "Goto Module" }
+      )
+      map(
+        "n",
+        "<leader>au",
+        "<cmd>:lua require('nvim-quick-switcher').find('*-routing.module.ts')<CR>",
+        { noremap = true, silent = true, desc = "Goto roUting module" }
+      )
+      map(
+        "n",
+        "<leader>ao",
+        "<cmd>:lua require('nvim-quick-switcher').find('.+model.ts|.+models.ts|.+types.ts', { regex = true })<CR>",
+        { noremap = true, silent = true, desc = "Goto mOdel" }
+      )
+      map(
+        "n",
+        "<leader>ar",
+        "<cmd>:lua require('nvim-quick-switcher').find('.service.ts')<CR>",
+        { noremap = true, silent = true, desc = "Goto seRvice" }
+      )
     end,
   },
 }
