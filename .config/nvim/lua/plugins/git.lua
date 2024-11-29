f0b1039fed012ee30a1a7e547535b42c962b50dd
Jack Pts <jackpts1981@gmail.com>
2024-11-29 18:06:50 +0300

    [nvim] [git] add diffView extended config

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,4 +1,8 @@
 local map = vim.keymap.set
+local function get_default_branch_name()
+  local res = vim.system({ "git", "rev-parse", "--verify", "main" }, { capture_output = true }):wait()
+  return res.code == 0 and "main" or "master"
+end
 
 return {
   -- Diff Git View open in New Buffer
@@ -7,7 +11,34 @@ return {
   -- >> The current file: :DiffviewFileHistory %
   -- Calling :DiffviewOpen with no args opens a new Diffview that compares against the current index
   -- You can also provide any valid git rev to view: :DiffviewOpen HEAD~2
-  { "sindrets/diffview.nvim" },
+  --
+  -- Detailed config taken from: https://github.com/bphkns/dotfiles/blob/main/nvim/.config/nvim/lua/plugins/diffview.lua
+  {
+    "sindrets/diffview.nvim",
+    lazy = true,
+    keys = {
+      { ",gd", "<cmd>DiffviewOpen<cr>", desc = "Open diffview" },
+      { ",gc", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
+      { ",gr", "<cmd>DiffviewFileHistory<cr>", desc = "Repo history" },
+      { ",gf", "<cmd>DiffviewFileHistory --follow %<cr>", desc = "File history" },
+      { ",gv", "<Esc><Cmd>'<,'>DiffviewFileHistory --follow<CR>", mode = "v", desc = "Range history" },
+      { ",gl", "<Cmd>.DiffviewFileHistory --follow<CR>", desc = "Line history" },
+      {
+        ",gm",
+        function()
+          vim.cmd("DiffviewOpen " .. get_default_branch_name())
+        end,
+        desc = "Compare local main",
+      },
+      {
+        ",gM",
+        function()
+          vim.cmd("DiffviewOpen HEAD..origin/" .. get_default_branch_name())
+        end,
+        desc = "Compare remote main",
+      },
+    },
+  },
 
   -- Git signs
   {
