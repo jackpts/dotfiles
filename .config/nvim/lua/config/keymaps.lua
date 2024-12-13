7e4910c7ffc05839f7febf704546aa0d708f5a29
Jack Pts <jackpts1981@gmail.com>
2024-12-13 17:24:39 +0300

    [nvim] keymaps for favourite colorschemes added

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -57,11 +57,29 @@ map("n", "yc", "yy<cmd>normal gcc<CR>p")
 map("n", "<Leader>r", [[:%s/\<<C-r><C-w>\>//g<Left><Left>]])
 
 wk.add({
+  -- Neogit
   { "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" },
   { "<leader>g;", "<cmd>lua require('neogit').action('log', 'log_current')()<cr>", desc = "Neogit logs" },
+  -- Diffview
+  { ",g", group = "Diffview" }, -- see git plugin to Full DiffView commands list
   { "<leader>gd", group = "Diffview" },
   { "<leader>gdc", "<cmd>DiffviewClose<cr>", mode = { "n", "i", "v" }, desc = "Close Diffview" },
   { "<leader>gdd", "<cmd>DiffviewOpen<cr>", mode = { "n", "i", "v" }, desc = "Open Diffview" },
   { "<leader>gdf", "<cmd>DiffviewToggleFiles<cr>", mode = { "n", "i", "v" }, desc = "Toggle Diffview file view" },
   { "<leader>gdr", "<cmd>DiffviewRefresh<cr>", mode = { "n", "i", "v" }, desc = "Refresh Diffview" },
+  -- Favourite ColorScheme
+  { ",c", group = "ColorScheme" },
+  { ",c1", ":colorscheme everforest<cr>", mode = { "n" }, desc = "everforest" },
+  { ",c2", ":colorscheme solarized-osaka<cr>", mode = { "n" }, desc = "osaka" },
+  { ",c3", ":colorscheme base16-icy<cr>", mode = { "n" }, desc = "icy" },
+  { ",c4", ":colorscheme base16-darkmoss<cr>", mode = { "n" }, desc = "darkmoss" },
+  { ",c5", ":colorscheme base16-black-metal-immortal<cr>", mode = { "n" }, desc = "black-metal" },
+  { ",c6", ":colorscheme duskfox<cr>", mode = { "n" }, desc = "duskfox" },
+  { ",c7", ":colorscheme tokyonight<cr>", mode = { "n" }, desc = "tokyonight" },
+  { ",c8", ":colorscheme base16-gruvbox-dark-medium<cr>", mode = { "n" }, desc = "gruvbox" },
+  { ",c9", ":colorscheme base16-catppuccin<cr>", mode = { "n" }, desc = "catppuccin" },
 })
+
+-- map H & L instead of ^ and $ to jump to the start/end of the line
+map("n", "H", "^")
+map("n", "L", "$")
