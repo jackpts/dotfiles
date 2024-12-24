3d1a28832692439f254c1fee10226c35b725f54d
Jack Pts <jackpts1981@gmail.com>
2024-12-24 22:39:30 +0300

    [hyprland] main menu & power menu beautification

--- .config/waybar/power_menu.sh
+++ .config/waybar/power_menu.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-chosen=$(echo -e "Shutdown\nReboot\nLock\nSuspend\nHybernate\nLogOut" | rofi -dmenu -i -p "Power")
+# chosen=$(echo -e "Shutdown\nReboot\nLock\nSuspend\nHybernate\nLogOut" | rofi -dmenu -i -p "Power")
+chosen=$(echo -e " Shutdown\n Reboot\n Lock\n Suspend\n LogOut" | rofi -dmenu -i -p "Power")
 
 case "$chosen" in
 Shutdown) systemctl poweroff ;;
 Reboot) systemctl reboot ;;
 Lock) swaylock ;;
 Suspend) systemctl suspend ;;
-Hybernate) systemctl hybernate ;;
+# Hybernate) systemctl hybernate ;;
 # LogOut) kill -1 -1 ;;
 LogOut) hyprctl dispatch exit ;;
 *) exit 1 ;;
