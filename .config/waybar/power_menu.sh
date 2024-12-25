bbd30b0258aede506cffea87e34f0c399f11a1a9
Jack Pts <jackpts1981@gmail.com>
2024-12-25 14:32:47 +0300

    [hyprland] add cyberpunk wall & face for hyprlock app, fix powerMenu script, fix styles for BT

--- .config/waybar/power_menu.sh
+++ .config/waybar/power_menu.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+shutdown=' Shutdown'
+reboot=' Reboot'
+lock=' Lock'
+suspend=' Suspend'
+logout=' LogOut'
+
 # chosen=$(echo -e "Shutdown\nReboot\nLock\nSuspend\nHybernate\nLogOut" | rofi -dmenu -i -p "Power")
-chosen=$(echo -e " Shutdown\n Reboot\n Lock\n Suspend\n LogOut" | rofi -dmenu -i -p "Power")
+chosen=$(echo -e "$shutdown\n$reboot\n$lock\n$suspend\n$logout" | rofi -dmenu -i -p "Power")
 
 case "$chosen" in
-Shutdown) systemctl poweroff ;;
-Reboot) systemctl reboot ;;
-Lock) swaylock ;;
-Suspend) systemctl suspend ;;
+$shutdown) systemctl poweroff ;;
+$reboot) systemctl reboot ;;
+$lock) swaylock ;;
+$suspend) systemctl suspend ;;
 # Hybernate) systemctl hybernate ;;
 # LogOut) kill -1 -1 ;;
-LogOut) hyprctl dispatch exit ;;
+$logout) hyprctl dispatch exit ;;
 *) exit 1 ;;
 esac
