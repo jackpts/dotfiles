633fc9dac0eff11efcfe57e6e42260ce41ac0c4f
Jack Pts <jackpts1981@gmail.com>
2024-12-25 14:38:02 +0300

    [hyprland] redundant comments removal, envs separation

--- .config/waybar/power_menu.sh
+++ .config/waybar/power_menu.sh
@@ -6,7 +6,6 @@ lock=' Lock'
 suspend=' Suspend'
 logout=' LogOut'
 
-# chosen=$(echo -e "Shutdown\nReboot\nLock\nSuspend\nHybernate\nLogOut" | rofi -dmenu -i -p "Power")
 chosen=$(echo -e "$shutdown\n$reboot\n$lock\n$suspend\n$logout" | rofi -dmenu -i -p "Power")
 
 case "$chosen" in
@@ -14,8 +13,6 @@ $shutdown) systemctl poweroff ;;
 $reboot) systemctl reboot ;;
 $lock) swaylock ;;
 $suspend) systemctl suspend ;;
-# Hybernate) systemctl hybernate ;;
-# LogOut) kill -1 -1 ;;
 $logout) hyprctl dispatch exit ;;
 *) exit 1 ;;
 esac
