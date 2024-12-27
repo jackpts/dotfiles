31986146a584de5dbef24afae6b6a1095b770eda
Jack Pts <jackpts1981@gmail.com>
2024-12-27 19:27:30 +0300

    [nvim] DBUI plugin add lazyness, cmd, nerd font toggle

--- .config/nvim/lua/plugins/sql.lua
+++ .config/nvim/lua/plugins/sql.lua
@@ -1,9 +1,21 @@
 -- Dad Bod Plugins, found by video: https://www.youtube.com/watch?v=ALGBuFLzDSA
 -- Commands:
 -- :DBUI, :tabnew, :DB mysql://jacky@localhost/
--- <Leader>S = run query, <Leader>W = save query
+--  <Leader>D = toggle DBUI, <Leader>S = run query, <Leader>W = save query
 return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
+  { "tpope/vim-dadbod", lazy = true },
+  { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    event = "VeryLazy",
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
