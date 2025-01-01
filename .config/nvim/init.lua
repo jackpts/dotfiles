050548fb54b5f4bace879256dd5395aed28f0d26
Jack Pts <jackpts1981@gmail.com>
2025-01-01 23:49:39 +0300

    [hyprland] add minimal backup style for rofi

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- require("neo-tree").setup({
+--     filesystem = {
+--         filtered_items = {
+--             hide_dotfiles = false, -- Show hidden files (dotfiles)
+--             hide_gitignored = false, -- Show files ignored by git
+--         },
+--     },
+-- })
+
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--     callback = function()
+--         local filepath = vim.fn.expand("%:p:h") -- Get the directory of the current file
+--         require("neo-tree.command").execute({
+--             action = "show",
+--             path = filepath,
+--         })
+--     end,
+-- })
