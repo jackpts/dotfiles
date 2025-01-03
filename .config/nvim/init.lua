b9b8d3cb109c22539e7a08e73a55cafcf931a6c4
Jack Pts <jackpts1981@gmail.com>
2025-01-04 01:15:49 +0300

    [nvim] neo-tree override to show hidden files from the start

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,39 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- require("neo-tree").setup({
---     filesystem = {
---         filtered_items = {
---             hide_dotfiles = false, -- Show hidden files (dotfiles)
---             hide_gitignored = false, -- Show files ignored by git
---         },
---     },
--- })
+require("neo-tree").setup({
+    filesystem = {
+        filtered_items = {
+            hide_dotfiles = false, -- Show hidden files (dotfiles)
+            hide_gitignored = true, -- Show files ignored by git
+            visible = true,
+        },
+        follow_current_file = true,
+    },
+    buffers = {
+        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        group_empty_dirs = true, -- when true, empty folders will be grouped together
+        show_unloaded = true,
+    },
+    git_status = {
+        window = {
+            position = "float",
+            mappings = {
+                ["A"] = "git_add_file",
+                ["a"] = "git_add_all",
+                ["X"] = "git_restore_file",
+                ["x"] = "git_restore_all",
+                ["u"] = "git_unstage_file",
+                ["D"] = "git_delete_file",
+                ["r"] = "git_rename_file",
+                ["<C-r>"] = "git_refresh_file",
+                ["o"] = "system_open",
+                ["y"] = "copy_to_clipboard",
+                ["I"] = "show_file_details",
+            },
+        },
+    },
+})
 
 -- vim.api.nvim_create_autocmd("BufEnter", {
 --     callback = function()
