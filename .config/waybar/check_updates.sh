9aff36b19b6e42b592a177c93bf709a71c6dc16b
Jack Pts <jackpts1981@gmail.com>
2025-01-06 20:48:12 +0300

    [hyprland] custom-updates roll back and add a bit of changes

--- .config/waybar/check_updates.sh
+++ .config/waybar/check_updates.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Use checkupdates to count available updates
-updates=$(checkupdates 2>/dev/null | wc -l)
+updates=$(checkupdates | awk {'print $1'})
+updatesCount=$(checkupdates 2>/dev/null | wc -l)
 
 # Define the icon (use an appropriate icon that you have available)
 icon="⟳" # Вы можете заменить этот символ на любой другой, например, из Font Awesome
 
 # Output JSON format for Waybar
-if [ "$updates" -gt 0 ]; then
-  echo "{\"text\": \"$icon $updates updates\", \"tooltip\": \"There are $updates updates available\", \"class\": \"updates-available\"}"
+if [ "$updatesCount" -gt 0 ]; then
+    # echo "{\"text\": \"$icon $updatesCount\", \"tooltip\": \"There are $updatesCount updates available:\n$updates\", \"class\": \"updates-available\"}"
+    echo "{\"text\": \"$icon $updatesCount\", \"tooltip\": \"There are $updatesCount updates available\", \"class\": \"updates-available\"}"
 else
-  echo "{\"text\": \"$icon No updates\", \"tooltip\": \"System is up to date\", \"class\": \"updates-none\"}"
+    echo "{\"text\": \"$icon \", \"tooltip\": \"System is up to date\", \"class\": \"updates-none\"}"
 fi
