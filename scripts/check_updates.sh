edcc72c4d396657052597a63cf65c5232704cc87
Jack Pts <jackpts1981@gmail.com>
2025-01-12 01:18:40 +0300

    [waybar] fix updates count for 1 empty line (trim)

--- scripts/check_updates.sh
+++ scripts/check_updates.sh
@@ -8,14 +8,14 @@ fi
 
 # Use checkupdates to count available updates
 updates=$(checkupdates 2>/dev/null)
-updatesCount=$(echo "$updates" | wc -l)
+updatesCount=$(echo "$updates" | awk 'NF' | wc -l)
 
 icon="⟳"
 
 # Output JSON format for Waybar
 if [ "$updatesCount" -gt 0 ]; then
     escaped_updates=$(echo "\n$updates" | sed ':a;N;$!ba;s/\n/\\n/g')
-    json_output=$(printf '{"text": "%s", "tooltip": "There are %d updates available:%s", "class": "updates-available"}' "$icon $updatesCount" "$updatesCount" "$escaped_updates")
+    json_output=$(printf '{"text": "%s", "tooltip": "There are %d update(s) available:%s", "class": "updates-available"}' "$icon $updatesCount" "$updatesCount" "$escaped_updates")
 else
     json_output=$(printf '{"text": "%s ", "tooltip": "System is up to date", "class": "updates-none"}' "$icon")
 fi
