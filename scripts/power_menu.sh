b74a4eceea1b9c1c8f2f379778d8b760eb554c60
Jack Pts <jackpts1981@gmail.com>
2025-01-14 16:45:01 +0300

    [hyprland] swaylock & wlogout removal

--- scripts/power_menu.sh
+++ scripts/power_menu.sh
@@ -10,12 +10,12 @@ suspend=' Suspend'
 logout=' LogOut'
 
 chosen=$(echo -e "$shutdown\n$reboot\n$lock\n$suspend\n$logout" |
-  rofi -dmenu -p "Uptime: $uptime" -mesg "Uptime: $uptime" -theme ${theme})
+    rofi -dmenu -p "Uptime: $uptime" -mesg "Uptime: $uptime" -theme ${theme})
 
 case "$chosen" in
 $shutdown) systemctl poweroff ;;
 $reboot) systemctl reboot ;;
-$lock) swaylock ;;
+$lock) $HOME/scripts/lock_with_matrix.sh ;;
 $suspend) systemctl suspend ;;
 $logout) hyprctl dispatch exit ;;
 *) exit 1 ;;
