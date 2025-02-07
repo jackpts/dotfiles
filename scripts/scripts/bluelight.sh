0e4209de14d98c574069e99b91ed93b5a91f650f
Jack Pts <jackpts1981@gmail.com>
2025-02-07 20:13:38 +0300

    [waybar] bluelight toggle by waybar icon click added

--- scripts/scripts/bluelight.sh
+++ scripts/scripts/bluelight.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-# STATE_FILE="$HOME/.config/waybar/bluelight_state"
-STATE_FILE="./bluelight_state"
+STATE_FILE="$HOME/.config/waybar/bluelight_state"
 
-if [ -f "$STATE_FILE" ]; then
-    killall hyprsunset
-    rm "$STATE_FILE"
-    hyprctl notify -1 3000 "rgb(ff1ea3)" "Blue Light Filter is OFF!"
-else
-    hyprsunset &
-    touch "$STATE_FILE"
-    hyprctl notify -1 3000 "rgb(ff1ea3)" "Blue Light Filter is ON!"
-fi
+toggle_state() {
+    if [ -f "$STATE_FILE" ]; then
+        killall hyprsunset
+        rm "$STATE_FILE"
+        hyprctl notify -1 3000 "rgb(ff1ea3)" "Blue Light Filter is OFF!"
+    else
+        hyprsunset &
+        touch "$STATE_FILE"
+        hyprctl notify -1 3000 "rgb(ff1ea3)" "Blue Light Filter is ON!"
+    fi
+}
+
+get_state() {
+    if [ -f "$STATE_FILE" ]; then
+        echo '{"text": "", "tooltip": "Blue Light Filter: ON", "class": "on"}'
+    else
+        echo '{"text": "", "tooltip": "Blue Light Filter: OFF", "class": "off"}'
+    fi
+}
+
+case "$1" in
+--toggle)
+    toggle_state
+    ;;
+*)
+    get_state
+    ;;
+esac
