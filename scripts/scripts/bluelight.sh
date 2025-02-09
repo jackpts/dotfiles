cc729afd342f4bab3aef5b5780b30ac6baf11ded
Jack Pts <jackpts1981@gmail.com>
2025-02-09 18:23:46 +0300

    [scripts] refactor bluelight state logic to get rid of file checking and using hyprsunset instead

--- scripts/scripts/bluelight.sh
+++ scripts/scripts/bluelight.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-STATE_FILE="$HOME/.config/waybar/bluelight_state"
-
 toggle_state() {
-    if [ -f "$STATE_FILE" ]; then
+    if pgrep -x "hyprsunset" >/dev/null; then
         killall hyprsunset
-        rm "$STATE_FILE"
         hyprctl notify -1 3000 "rgb(ff1ea3)" "Blue Light Filter is OFF!"
     else
         hyprsunset &
-        touch "$STATE_FILE"
         hyprctl notify -1 3000 "rgb(ff1ea3)" "Blue Light Filter is ON!"
     fi
 }
 
 get_state() {
-    if [ -f "$STATE_FILE" ]; then
+    if pgrep -x "hyprsunset" >/dev/null; then
         echo '{"text": "", "tooltip": "Blue Light Filter: ON", "class": "on"}'
     else
         echo '{"text": "", "tooltip": "Blue Light Filter: OFF", "class": "off"}'
