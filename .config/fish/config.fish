32bd339bfb44be14d4cde866e83977a0b948aca4
Jack Pts <jackpts1981@gmail.com>
2025-02-21 14:54:37 +0300

    [fish] sddm restart abbr added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -159,6 +159,7 @@ abbr pull_fotos 'adb pull /sdcard/DCIM/Camera/ ~/Downloads/'
 abbr rs "rsync -avh --progress --exclude 'node_modules'"
 abbr sddm_list 'ls -1 /usr/share/sddm/themes'
 abbr sddm_edit 'nvim /etc/sddm.conf'
+abbr sddm_restart 'sudo systemctl restart sddm'
 abbr sddm_theme 'bash $HOME/scripts/sddm_setup_theme.sh'
 abbr plym_edit 'nvim /etc/plymouth/plymouthd.conf'
 abbr plym_update 'sudo mkinitcpio -P'
