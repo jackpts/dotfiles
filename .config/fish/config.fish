5cdebf28539a154a2ea0af7aee64b8e3653f4b5b
Jack Pts <jackpts1981@gmail.com>
2025-02-21 14:50:09 +0300

    [fish] sddm list abbr added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -157,6 +157,7 @@ abbr -a -g priv 'fish --private'
 abbr -a -g genpass 'openssl rand -base64 10'
 abbr pull_fotos 'adb pull /sdcard/DCIM/Camera/ ~/Downloads/'
 abbr rs "rsync -avh --progress --exclude 'node_modules'"
+abbr sddm_list 'ls -1 /usr/share/sddm/themes'
 abbr sddm_edit 'nvim /etc/sddm.conf'
 abbr sddm_theme 'bash $HOME/scripts/sddm_setup_theme.sh'
 abbr plym_edit 'nvim /etc/plymouth/plymouthd.conf'
