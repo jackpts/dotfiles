3b66f8a5778baff6aaf9adfc9d10a22e2275459c
Jack Pts <jackpts1981@gmail.com>
2025-03-04 01:50:58 +0300

    [nvim] packageInfo add more options

--- .config/nvim/lua/plugins/package-info.lua
+++ .config/nvim/lua/plugins/package-info.lua
@@ -7,8 +7,11 @@ return {
 
         keys = {
             { "<leader>ps", "<cmd>lua require('package-info').show()<cr>", desc = "PackageInfo show" },
+            { "<leader>ph", "<cmd>lua require('package-info').hide()<cr>", desc = "PackageInfo hide" },
             { "<leader>pd", "<cmd>lua require('package-info').delete()<cr>", desc = "PackageInfo delete" },
             { "<leader>pi", "<cmd>lua require('package-info').install()<cr>", desc = "PackageInfo install" },
+            { "<leader>pt", "<cmd>lua require('package-info').toggle()<cr>", desc = "PackageInfo toggle" },
+            { "<leader>pu", "<cmd>lua require('package-info').update()<cr>", desc = "PackageInfo update" },
             {
                 "<leader>pc",
                 "<cmd>lua require('package-info').change_version()<cr>",
