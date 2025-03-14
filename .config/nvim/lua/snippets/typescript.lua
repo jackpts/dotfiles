e0ef6cbb52076bc6f6e0420ce87e3eb49163c1f2
Jack Pts <jackpts1981@gmail.com>
2025-03-14 17:14:18 +0300

    [nvim] fix nvim ts snippet

--- .config/nvim/lua/snippets/typescript.lua
+++ .config/nvim/lua/snippets/typescript.lua
@@ -1,21 +1,55 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("typescript", {
     s("cl", {
         t("console.log('text');"),
     }),
-    s("if", {
-        t("if (${1:condition}) {\n\t$0\n}"),
-    }),
-    s("fun", {
-        t("function ${1:name}(${2:args}) {\n\t$0\n}"),
-    }),
-    s("import", {
-        t("import { ${2:obj} } from '${1:package}';"),
-    }),
-    s("interface", {
-        t("interface ${1:InterfaceName} {\n\t${2:property}: ${3:type};\n}"),
-    }),
+    s(
+        { trig = "fun", dscr = "Function declaration" },
+        fmt(
+            [[
+      function {}({}) {{
+      {}    {}
+      }}
+      ]],
+            {
+                i(1, "name"),
+                i(2, "args"),
+                t("\t"),
+                i(0),
+            }
+        )
+    ),
+    s(
+        { trig = "import", dscr = "Import statement" },
+        fmt(
+            [[
+      import {{ {} }} from "{}";
+      ]],
+            {
+                i(2, "obj"),
+                i(1, "package"),
+            }
+        )
+    ),
+    s(
+        { trig = "interface", dscr = "Interface declaration" },
+        fmt(
+            [[
+      interface {} {{
+      {}{}: {};
+      }}
+      ]],
+            {
+                i(1, "InterfaceName"),
+                t("\t"),
+                i(2, "property"),
+                i(3, "type"),
+            }
+        )
+    ),
 })
