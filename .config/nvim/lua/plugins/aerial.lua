9e01c05cdb62a4077bfa09b45d73f1b3265fe997
Jack Pts <jackpts1981@gmail.com>
2025-03-14 17:45:42 +0300

    [nvim] add aerial nav plugin

---
+++ .config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,16 @@
+return {
+	'stevearc/aerial.nvim',
+	opts = {},
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons"
+	},
+    cmd = {
+      "AerialOpen",
+      "AerialClose",
+      "AerialToggle",
+      "AerialInfo",
+      "AerialPrev",
+      "AerialNext",
+    },
+}
