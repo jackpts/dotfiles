2c62286cd20eda1ee664bb7359deb00fb7183375
Jack Pts <jackpts1981@gmail.com>
2025-03-14 16:48:55 +0300

    [nvim] update nvim ts snippet

--- .config/nvim/lua/snippets/typescript.lua
+++ .config/nvim/lua/snippets/typescript.lua
@@ -3,7 +3,19 @@ local s = ls.snippet
 local t = ls.text_node
 
 ls.add_snippets("typescript", {
-  s("cl", {
-    t("console.log('text');"),
-  }),
+    s("cl", {
+        t("console.log('text');"),
+    }),
+    s("if", {
+        t("if (${1:condition}) {\n\t$0\n}"),
+    }),
+    s("fun", {
+        t("function ${1:name}(${2:args}) {\n\t$0\n}"),
+    }),
+    s("import", {
+        t("import { ${2:obj} } from '${1:package}';"),
+    }),
+    s("interface", {
+        t("interface ${1:InterfaceName} {\n\t${2:property}: ${3:type};\n}"),
+    }),
 })
