eb61034113ad5914ee4719513973eaa9d2405ad7
Jack Pts <jackpts1981@gmail.com>
2025-03-20 12:17:01 +0300

    [nvim] rust plugin added

---
+++ .config/nvim/lua/plugins/rust.lua
@@ -0,0 +1,9 @@
+return {
+    -- Default rust LSP kinda sucks so we use this plugin instead
+    {
+        "mrcjkb/rustaceanvim",
+        ft = "rust",
+        version = "^4",
+        enabled = false,
+    },
+}
