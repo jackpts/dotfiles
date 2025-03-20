5b1cdb4359ec7239e2cc8fbb85e0ac0cb5d8d08a
Jack Pts <jackpts1981@gmail.com>
2025-03-20 12:17:58 +0300

    [nvim] autocmd config up - added case w/ dir autocreation

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local fn = vim.fn
+
+-- https://www.reddit.com/r/neovim/comments/1i2xw2m/comment/m7jbnnf/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+autocmd("BufWritePre", {
+	desc = "Autocreate a dir when saving a file",
+	group = augroup("auto_create_dir", { clear = true }),
+	callback = function(event)
+		if event.match:match("^%w%w+:[\\/][\\/]") then
+			return
+		end
+		local file = vim.uv.fs_realpath(event.match) or event.match
+		fn.mkdir(fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
