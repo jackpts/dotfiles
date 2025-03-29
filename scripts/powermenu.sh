9acf883ad12245bfc4070b9b5df28f630bd057a4
Jack Pts <jackpts1981@gmail.com>
2025-03-29 22:50:00 +0300

    [scripts][rofi] fix powermenu appear on prev active workspace glitch

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -27,6 +27,7 @@ rofi_cmd() {
 
 # Confirmation CMD
 confirm_cmd() {
+    current_workspace=$(hyprctl activeworkspace -j | jq -r '.id')
     rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
         -theme-str 'mainbox {children: [ "message", "listview" ];}' \
         -theme-str 'listview {columns: 2; lines: 1;}' \
@@ -36,7 +37,9 @@ confirm_cmd() {
         -p 'Confirmation' \
         -mesg 'Are you Sure?' \
         -theme ${dir}/${theme}.rasi \
-        -display-drv wayland
+        -display-drv wayland &
+    rofi_pid=$!
+    hyprctl dispatch movetoworkspace $current_workspace,pid:$rofi_pid
 }
 
 # Pass variables to rofi dmenu
