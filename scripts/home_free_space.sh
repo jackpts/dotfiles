d954aa5ecdd9e1602310ea4f756eb38750db4f4b
Jack Pts <jackpts1981@gmail.com>
2025-04-16 16:17:19 +0300

    [scripts][waybar] free_space script icon removal

--- scripts/home_free_space.sh
+++ scripts/home_free_space.sh
@@ -3,9 +3,9 @@
 free_space=$(df -h /home | awk 'NR==2 {print $4}' | sed 's/G//')
 
 if [ "$free_space" -lt 10 ]; then
-    output=$(printf '{"text": "%s", "class": "exceed"}' "  $free_space Gb")
+    output=$(printf '{"text": "%s", "class": "exceed"}' "$free_space Gb")
 else
-    output=$(printf '{"text": "%s", "class": "default"}' "  $free_space Gb")
+    output=$(printf '{"text": "%s", "class": "default"}' "$free_space Gb")
 fi
 
 echo "$output"
