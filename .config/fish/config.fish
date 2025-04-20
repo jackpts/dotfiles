bccd5c7a71231ec30487bfc12fdb4bc580ebe969
Jack Pts <jackpts1981@gmail.com>
2025-04-21 00:00:28 +0300

    fish disable old abbrs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,12 +8,12 @@ set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 #   buildin source $file 2>/dev/null
 # end
 
-if not functions -q fisher
-    echo "Installing fisher for the first time..." >&2
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fisher
-end
+# if not functions -q fisher
+#     echo "Installing fisher for the first time..." >&2
+#     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+#     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+#     fisher
+# end
 
 # theme
 set -g theme_display_git yes
