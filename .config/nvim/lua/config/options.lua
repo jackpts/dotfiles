64189d541523e88a4ec283bd649205f17c1c4126
Jack Pts <jackpts1981@gmail.com>
2025-04-22 01:55:30 +0300

    [nvim] options up

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,13 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local opt = vim.opt
+
 vim.o.swapfile = false -- disable swapfiles because they are fucking garbage
 vim.o.shell = "/usr/bin/zsh"
-vim.opt.relativenumber = false
-vim.opt.clipboard = "unnamedplus" -- Use system clipboard
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.autoindent = true
+opt.relativenumber = false
+opt.clipboard = "unnamedplus" -- Use system clipboard
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+opt.smartcase = true
+opt.incsearch = true
+opt.autoindent = true
+opt.fillchars = {eob = " "}
 
-vim.wo.wrap = true
+vim.wo.wrap = false
 vim.g.autoformat = false
+vim.o.signcolumn = "yes:1"
