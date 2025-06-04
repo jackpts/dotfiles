887de2aa87e8141104bda5e70a4e6fc040a2e304
Jack Pts <jackpts1981@gmail.com>
2025-06-04 15:59:01 +0300

    [mpvlock] uptime script rework

--- .config/mpvlock/scripts/uptime.sh
+++ .config/mpvlock/scripts/uptime.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Check if the "uptime" command exists and is executable
-if ! command -v uptime &>/dev/null; then
-    echo "Error: The 'uptime' command could not be found."
+# Check if the "awk" and "bc" commands exist and are executable
+if ! command -v awk &>/dev/null || ! command -v bc &>/dev/null; then
+    echo "Error: The 'awk' and/or 'bc' commands could not be found."
     exit 1
 fi
 
-# Get system uptime information
-# uptime_info=$(uptime)
-uptime_info=$(uptime -p | sed -e 's/up //g')
+# Get system uptime information in seconds
+uptime_seconds=$(awk '{print int($1)}' /proc/uptime)
+
+# Calculate hours and minutes using bc for integer division
+hours=$((uptime_seconds/3600))
+minutes=$(((uptime_seconds%3600)/60))
+
+# Format the time as "HH:MM"
+formatted_time=$(printf "%02d:%02d\n" $hours $minutes)
 
 # Print the uptime with a formatted message
-printf "System Uptime: "
-printf "%s\n" "$uptime_info"
+printf "Uptime: "
+printf "%s\n" "$formatted_time"
 
 exit 0
