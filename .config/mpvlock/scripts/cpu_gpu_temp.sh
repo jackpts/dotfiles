2697193d21a31867a59ddcece0556aca8b1b551e
Jack Pts <jackpts1981@gmail.com>
2025-06-04 15:58:31 +0300

    [mpvlock] cpu/gpu temp round

--- .config/mpvlock/scripts/cpu_gpu_temp.sh
+++ .config/mpvlock/scripts/cpu_gpu_temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cpu_temp=$(awk '{print $1/1000}' /sys/class/hwmon/hwmon4/temp1_input | bc -l)
-gpu_temp=$(awk '{print $1/1000}' /sys/class/hwmon/hwmon5/temp1_input | bc -l)
+cpu_temp=$(printf "%.0f" $(awk '{print $1/1000}' /sys/class/hwmon/hwmon4/temp1_input))
+gpu_temp=$(printf "%.0f" $(awk '{print $1/1000}' /sys/class/hwmon/hwmon5/temp1_input))
 
-echo "CPU/GPU Temp: ${cpu_temp} / ${gpu_temp}"
+echo "CPU/GPU °C: ${cpu_temp} / ${gpu_temp}"
 
