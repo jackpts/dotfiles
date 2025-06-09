fbcb62289d95a3b1c06018d2f2c8cd25112c818d
Jack Pts <jackpts1981@gmail.com>
2025-06-09 17:18:54 +0300

    [nvim] animated dashboard added

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,18 +1,342 @@
+local shouldPlayAnimation = true
+
+-- The ASCII caracters used in the animation
+-- ⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏
+-- ⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟
+-- ⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯
+-- ⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿
+-- ⡀⡁⡂⡃⡄⡅⡆⡇⡈⡉⡊⡋⡌⡍⡎⡏
+-- ⡐⡑⡒⡓⡔⡕⡖⡗⡘⡙⡚⡛⡜⡝⡞⡟
+-- ⡠⡡⡢⡣⡤⡥⡦⡧⡨⡩⡪⡫⡬⡭⡮⡯
+-- ⡰⡱⡲⡳⡴⡵⡶⡷⡸⡹⡺⡻⡼⡽⡾⡿
+-- ⢀⢁⢂⢃⢄⢅⢆⢇⢈⢉⢊⢋⢌⢍⢎⢏
+-- ⢐⢑⢒⢓⢔⢕⢖⢗⢘⢙⢚⢛⢜⢝⢞⢟
+-- ⢠⢡⢢⢣⢤⢥⢦⢧⢨⢩⢪⢫⢬⢭⢮⢯
+-- ⢰⢱⢲⢳⢴⢵⢶⢷⢸⢹⢺⢻⢼⢽⢾⢿
+-- ⣀⣁⣂⣃⣄⣅⣆⣇⣈⣉⣊⣋⣌⣍⣎⣏
+-- ⣐⣑⣒⣓⣔⣕⣖⣗⣘⣙⣚⣛⣜⣝⣞⣟
+-- ⣤⣡⣢⣣⣤⣥⣦⣧⣨⣩⣪⣫⣬⣭⣮⣯
+-- ⣰⣱⣲⣳⣴⣵⣶⣷⣸⣹⣺⣻⣼⣽⣾⣿
+
+local frames = {
+  [[
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀ ⢠⡄⠀ ⠀⠀ ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀ ⣠⣿⣿⣆ ⠀⠀⠀⠀⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀ ⡰⠿⠿⠿⠿⢆⠀ ⠀⠀⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀ ⣴⣷⡀⠀⠀⢀⣾⣦⠀⠀ ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀ ⣼⣿⣿⣷⡀⢀⣾⣿⣿⣧  ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀ ⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁ ⠀⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀ ⢠⡄⠀ ⠀⠀ ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀ ⣠⣿⣿⣆ ⠀⠀⠀⠀⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀ ⡰⠿⠿⠿⠿⢆⠀ ⠀⠀⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀ ⣴⣷⡀⠀⠀⢀⣾⣦⠀⠀ ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀ ⣼⣿⣿⣷⡀⢀⣾⣿⣿⣧  ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀ ⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁ ⠀⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀ ⢠⡄⠀ ⠀⠀ ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀ ⣠⣿⣿⣆ ⠀⠀⠀⠀⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀ ⡰⠿⠿⠿⠿⢆⠀ ⠀⠀⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀ ⣴⣷⡀⠀⠀⢀⣾⣦⠀⠀ ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀ ⣼⣿⣿⣷⡀⢀⣾⣿⣿⣧  ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀ ⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁ ⠀⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀ ⢠ ⠀ ⠀⠀ ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀  ⣾⣇ ⠀⠀ ⠀⠀⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀  ⡰⠿⠿⢆⠀ ⠀ ⠀⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀  ⣴⣷ ⠀⣾⣦⠀⠀  ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀  ⣼⣿⣷⡀⢀⣿⣿⣧   ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀  ⠈⠉⠉⠉⠉⠉⠉⠉⠉⠁  ⠀⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀  
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀ ⢠⠀ ⠀⠀  ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀  ⣾⣇ ⠀⠀ ⠀⠀⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀  ⡰⠿⠿⠀ ⠀ ⠀ ⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀  ⢀⣷  ⣷⠀⠀   ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀   ⣿⣿⡀⢀⣿⣧    ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀   ⠉⠉⠉⠉⠉⠉⠉⠁   ⠀⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀  
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀  ⢠⠀⠀ ⠀  ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀  ⣾ ⠀ ⠀ ⠀ ⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀  ⡰⠿⠀ ⠀⠀   ⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀   ⣷ ⣷⠀⠀    ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀   ⡰⣿⢀⣿      ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀    ⠉⠉⠉⠉⠁   ⠀  ⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀ ⠀⢠⡄ ⠀ ⠀ ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀  ⢸⡇ ⠀ ⠀ ⠀⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀   ⠸⠇⠀  ⠀⠀ ⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀    ⢸⡆⠀     ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀     ⢸⡇      ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀      ⠈⠁    ⠀  ⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀  
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀ ⠀⠀ ⡄⠀⠀   ⠈⢿⣿⣏⢀⡀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀ ⠀  ⣿ ⠀⠀  ⠀⠈⢿⣿⡁ ⣁⡀⠀ ⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀ ⠀   ⠿⢆⠀⠀⠀  ⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀    ⣾ ⣾⠀    ⠀⠀⣿⣿⡦⠀⠀⠀⠀ 
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀     ⣿⡀⣿⢆    ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀      ⠉⠉⠉⠉  ⠀  ⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀  
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀  ⡄⠀ ⠀⠀ ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀  ⣰⣿ ⠀⠀ ⠀ ⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀   ⠿⠿⢆ ⠀ ⠀ ⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀   ⣾  ⣾⡀⠀   ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀   ⣼⣿⡀⢀⣿⣿    ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀   ⠈⠉⠉⠉⠉⠉⠉⠉   ⠀⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+  [[
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⢀⣴⡖ ⠀⢀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀ ⣹⣷ ⣀⣴⠀⢀⣤⣶⡦⡀⠀⠀⠀⠀ ⠀
+  ⠢⣀⠀⠀⠀⠀⠀⢄⠀⠈⣆⣠⣼⣿⣿⣿⣿⣿⣿⣦⣼⣏⠀⠀⠀⠀⠀⠀  ⠀
+  ⠀⠈⠻⣶⣄⡀⠀⣨⣷⡿⠟⠋⠉⠉  ⠉⠉⠉⠛⠿⣿⣦⣀⠀⠀⠀⠀⠀ ⠀
+  ⠀⠀⠀⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀  ⡄⠀ ⠀⠀ ⠈⢿⣿⣏⢀⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⡟⠀⠀⠀⠀⠀  ⣰⣿ ⠀⠀ ⠀⠀⠈⢿⣿⡁ ⣁⡀⠀⠀
+  ⠀⠀⠀⠒⢺⣿⠁⠀⠀⠀⠀  ⡰⠿⠿⢆⠀ ⠀ ⠀⠀⠸⣿⡿⠛⠉⠀⠀⠀
+  ⠀⠀⢀⢠⣾⣿⠀⠀⠀⠀  ⣴⣷ ⠀⣾⣦⠀⠀  ⠀⠀⣿⣿⡦⠀⠀⠀⠀
+  ⠀⠀⠀⠀⣹⣿⠀⠀⠀  ⣼⣿⣿⡀⢀⣿⣿⣧   ⠀⢠⣿⡧⠤ ⠀⠀⠀
+  ⠀⠀⠀⠈⠛⢿⣆⠀  ⠈⠉⠉⠉⠉⠉⠉⠉⠉⠁  ⠀⣼⡿⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣾⣿⣷⣄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠚⢻⣿⣿⣦⣄⡀⠀⠀⠀⠀  ⢀⣠⣶⣿⡋⠀⠈⠙⠻⣄⠀⠀ 
+  ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠿⢿⣿⣿⣿⣶⣶⣿⡿⠻⣏⠀⠛⠄⠀⠀⠀⠀⠈⠃⠄ 
+  ⠀⠀⠀⠀⠀ ⡰⠛ ⠀⠸⠋⠻⣿⣁⠀⠁⠀⠀⠈⠀  ⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀
+
+]],
+}
+
+local asciiImg = frames[1]
+
+local function ascii(counting, callback)
+  if not shouldPlayAnimation then
+    return
+  end
+
+  -- local frameCount = #frames < math.floor(counting) and frames[#frames] or frames[math.floor(counting)]
+  asciiImg = #frames < math.floor(counting) and frames[#frames] or frames[math.floor(counting)]
+  Snacks.dashboard.update()
+
+  -- print(counting)
+
+  if counting >= #frames + 1 then
+    callback(callback)
+  end
+end
+
+local function theAnimation(callback)
+  require("snacks").animate(1, #frames + 1, function(value, ctx)
+    ascii(value, callback)
+  end, {
+    duration = 300,
+    fps = 60,
+  })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "snacks_dashboard",
+  callback = function()
+    -- Your custom code here that runs when the snacks dashboard filetype is set
+    -- print("Snacks Dashboard is open!")
+    shouldPlayAnimation = true
+    asciiImg = frames[1]
+  end,
+})
+
+vim.api.nvim_create_autocmd("DirChanged", {
+  pattern = "*", -- Match any directory change
+  callback = function()
+    local new_dir = vim.fn.getcwd()
+
+    -- Same logic as above for project markers
+    local project_markers = { ".git", "package.json", "Cargo.toml", ".project_root" }
+    local is_project_root = false
+
+    for _, marker in ipairs(project_markers) do
+      if vim.fn.findfile(marker, new_dir .. ";") ~= "" then
+        is_project_root = true
+        break
+      end
+    end
+
+    if is_project_root then
+      -- print("Changed to project directory: " .. new_dir)
+      shouldPlayAnimation = false
+      -- Your custom code here for when you change into a project directory
+    else
+      -- print("Changed to non-project directory: " .. new_dir)
+      shouldPlayAnimation = false
+      -- Optional: Code for when you leave a project directory
+    end
+  end,
+})
+
 return {
   { "nvimdev/dashboard-nvim", enabled = false },
 
   {
     "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
+    -- priority = 1000,
+    -- lazy = false,
+
     ---@type snacks.Config
     opts = {
+      image = { enabled = true },
+
+      -- DASHBOARD
       dashboard = {
         enabled = true,
+        preset = {
+          header = false,
+          pick = nil,
+          keys = {
+            {
+              icon = " ",
+              key = "p",
+              desc = "Projects",
+              action = function()
+                Snacks.picker.projects({
+                  sort = { fields = { "time:asc", "idx" } },
+                  on_close = function(info)
+                    shouldPlayAnimation = true
+                  end,
+                })
+                shouldPlayAnimation = false
+              end,
+            },
+            {
+              icon = " ",
+              key = "s",
+              desc = "Restore Session",
+              action = function()
+                require("persistence").load({ last = true })
+                shouldPlayAnimation = false
+              end,
+            },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = function()
+                Snacks.dashboard.pick("files", {
+                  cwd = vim.fn.stdpath("config"),
+                  on_close = function(info)
+                    shouldPlayAnimation = true
+                  end,
+                })
+                shouldPlayAnimation = false
+              end,
+            },
+            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
         sections = {
-          { section = "header" },
+          {
+            section = "header",
+            padding = 1,
+            function()
+              return { header = asciiImg }
+            end,
+          },
+          -- { section = "header" },
           -- { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
-          { section = "keys", gap = 1, padding = 1 },
+          { section = "keys", gap = 0, padding = 1 },
           { pane = 1, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           {
@@ -27,9 +351,14 @@ return {
             ttl = 5 * 60,
             indent = 3,
           },
-          { section = "startup" },
+          -- { section = "startup" },
+          { section = "startup", enabled = false },
         },
+        on_close = function()
+          shouldPlayAnimation = false
+        end,
       },
+
       bigfile = { enabled = true },
       notifier = {
         enabled = true,
@@ -37,13 +366,19 @@ return {
       },
       quickfile = { enabled = true },
       statuscolumn = { enabled = true },
+
+      -- Terminal
       terminal = {
         enabled = true,
         win = {
           position = "float",
           border = "single",
+          keys = {
+            exit = { "<ESC>", "<cmd>q<cr>", desc = "Exit", expr = true, mode = { "t", "n" } },
+          },
         },
       },
+
       words = {
         enabled = true,
         debounce = 200,
@@ -165,6 +500,8 @@ return {
         },
       },
     },
+
+    -- INIT   
     init = function()
       vim.api.nvim_create_autocmd("User", {
         pattern = "VeryLazy",
@@ -192,6 +529,10 @@ return {
           Snacks.toggle.inlay_hints():map("<leader>uh")
         end,
       })
+
+      vim.defer_fn(function()
+        theAnimation(theAnimation)
+      end, 100)
     end,
   },
 }
