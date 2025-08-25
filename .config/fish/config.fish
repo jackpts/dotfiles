b9b9e2127352a2077f202f0c018e81f976b93c34
Jack Pts <jackpts1981@gmail.com>
2025-08-25 17:00:51 +0300

    [fish] config rework with GPT5

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,8 +13,8 @@ end
 
 ### NVM
 # bash /usr/share/nvm/init-nvm.sh
-# export NVM_DIR="$(printf %s "$HOME/.nvm")"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# set -gx NVM_DIR (printf %s "$HOME/.nvm")
+# test -s "$NVM_DIR/nvm.sh"; and source "$NVM_DIR/nvm.sh"
 
 ### pnpm
 set -gx PNPM_HOME "/home/jacky/.local/share/pnpm"
@@ -22,36 +22,36 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 
-### Proton
-export WLR_BACKEND=vulkan
-export __VK_LAYER_NV_optimus=NVIDIA_only
-export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
-export MESA_LOADER_DRIVER_OVERRIDE=zink
+### Proton / Graphics (commented out for stability)
+# export WLR_BACKEND=vulkan
+# export __VK_LAYER_NV_optimus=NVIDIA_only
+# export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+# export MESA_LOADER_DRIVER_OVERRIDE=zink
 
 ### BASE ALIASES
-alias cp="cp -i"
-alias df='df -h'
-alias free='free -m'
-alias qq='notepadqq $1 --allow-root'
-alias cls='clear'
-alias la='ls -A'
-alias l='ls -alF'
-alias cd..='cd ..'
+alias cp "cp -i"
+alias df 'df -h'
+alias free 'free -m'
+alias cls 'clear'
+alias la 'ls -A'
+alias l 'ls -alF'
+alias cd.. 'cd ..'
 abbr .. 'cd ..'
 abbr ... 'cd ../..'
-abbr ... 'cd ../../..'
+abbr .... 'cd ../../..'
 abbr mkdir 'mkdir -p'
-alias z..='z ..'
-alias logmeout='sudo pkill -u jacky'
-alias pacman_clean='pacman -Rsn $(pacman -Qdtq)'
-alias pacman_clear_cache='sudo paccache -r'
-alias gnome_startup='gnome-session-properties'
-alias rel_info='cat /etc/*rel*'
+alias z.. 'z ..'
+alias logmeout "sudo pkill -u $USER"
+alias pacman_clean "pacman -Rsn (pacman -Qdtq)"
+alias pacman_clear_cache 'sudo paccache -r'
+alias gnome_startup 'gnome-session-properties'
+alias rel_info 'cat /etc/*rel*'
 abbr duf 'duf --hide loops,special --sort filesystem'
 abbr themes 'ls /usr/share/themes'
 
 ### Errors
 abbr logs 'journalctl -xe'
+abbr log_errors 'journalctl -p err -b'
 abbr syslog 'sudo dmesg --level=emerg,alert,crit'
 abbr sys_acpi 'sudo dmesg | grep ACPI'
 
@@ -63,15 +63,14 @@ abbr gnome_ver 'gnome-shell --version'
 # abbr ext_edit 'nvim ~/github/gnome.ext.song.title/extension.js'
 # abbr ext_install 'gnome-extensions install ./gnome.ext.song.title.zip --force'
 # abbr ext_remove 'gnome-extensions uninstall gnome.ext.song.title@jackpts.github.com'
-# abbr ext_pack 'z ~/github && rm gnome.ext.song.title.zip && zip -r gnome.ext.song.title.zip ./gnome.ext.song.title -x "*.git*"'
+# abbr ext_pack 'z ~/github; and rm gnome.ext.song.title.zip; and zip -r gnome.ext.song.title.zip ./gnome.ext.song.title -x "*.git*"'
 # abbr ext_enable 'gnome-extensions enable gnome.ext.song.title@jackpts.github.com'
 # abbr ext_disable 'gnome-extensions disable gnome.extsong.title@jackpts.github.com'
 # abbr ext_go 'cd /home/jacky/.local/share/gnome-shell/extensions/'
-# abbr ext_reload 'gnome-extensions disable gnome.extsong.title@jackpts.github.com && \
-# gnome-extensions uninstall gnome.ext.song.title@jackpts.github.com && \
-# z ~/github && rm gnome.ext.song.title.zip && zip -r gnome.ext.song.title.zip ./gnome.ext.song.title -x "*.git*" && \
-# gnome-extensions install ./gnome.ext.song.title.zip --force
-# '
+# abbr ext_reload 'gnome-extensions disable gnome.extsong.title@jackpts.github.com; and \
+# gnome-extensions uninstall gnome.ext.song.title@jackpts.github.com; and \
+# z ~/github; and rm gnome.ext.song.title.zip; and zip -r gnome.ext.song.title.zip ./gnome.ext.song.title -x "*.git*"; and \
+# gnome-extensions install ./gnome.ext.song.title.zip --force'
 
 ### Hyprland
 abbr h_clients 'hyprctl clients'
@@ -115,90 +114,94 @@ function w_styles
     cd "$HOME/dotfiles/.config/waybar"
     sass style.scss style.css
 
-    sed '/@charset "UTF-8";/d' style.css >/tmp/style.css && mv /tmp/style.css style.css
+    sed '/@charset "UTF-8";/d' style.css >/tmp/style.css; and mv /tmp/style.css style.css
 end
 
 ### SETS
-set HISTSIZE -1 # Infinte history
-set HISTFILESIZE -1 # Infinte history
+set HISTSIZE -1 # Infinite history
+set HISTFILESIZE -1 # Infinite history
 set HISTCONTROL ignoreboth # Don't save duplicate commands or commands starting with space
 set -U -x TERMINAL kitty
 set -U XDG_DATA_HOME $HOME/.local/share
 
 # For Opencode:
 # set LOCAL_ENDPOINT http://localhost:1234/v1
-# export LOCAL_ENDPOINT="http://localhost:1234/v1"
-# export BASE_URL="http://localhost:1234/v1"
+# set -gx LOCAL_ENDPOINT http://localhost:1234/v1
+# set -gx BASE_URL http://localhost:1234/v1
 
 ### AI
 # For Aider:
-export OLLAMA_API_BASE=http://127.0.0.1:11434
-abbr ol_serve OLLAMA_CONTEXT_LENGTH=8192 OLLAMA_ACCELERATE=1 ollama serve
-abbr ai_gemma OLLAMA_ACCELERATE=1 aider --model ollama_chat/gemma3
-abbr ai_qwen OLLAMA_ACCELERATE=1 aider --model ollama_chat/qwen3
-export AIDER_DARK_MODE=true
+set -gx OLLAMA_API_BASE http://127.0.0.1:11434
+abbr ol_serve 'OLLAMA_CONTEXT_LENGTH=8192 OLLAMA_ACCELERATE=1 ollama serve'
+abbr ai_gemma 'OLLAMA_ACCELERATE=1 aider --model ollama_chat/gemma3'
+abbr ai_qwen 'OLLAMA_ACCELERATE=1 aider --model ollama_chat/qwen3'
+set -gx AIDER_DARK_MODE true
 # warp_cli
 abbr warp_cli $HOME/dotfiles/.venv/bin/python $HOME/dotfiles/scripts/warp-cli.py
 # Kiro
 abbr kiro ELECTRON_OZONE_PLATFORM_HINT=auto $HOME/soft/Kiro/kiro
 
-# Wayland
-set -gx MOZ_ENABLE_WAYLAND 1
-set -gx QT_QPA_PLATFORM wayland
-set -gx GDK_BACKEND wayland
+# Wayland (commented out for safety; set per-app if needed)
+# set -gx MOZ_ENABLE_WAYLAND 1
+# set -gx QT_QPA_PLATFORM wayland
+# set -gx GDK_BACKEND wayland
 
 # Fix error: X11Helper-WARNING **: 01:47:27.455: Failed to open display: :1
 # echo $DISPLAY
 # :1
 # set -e DISPLAY
 # set -gx DISPLAY eDP-1
-set -gx DISPLAY $WAYLAND_DISPLAY
+# set -gx DISPLAY $WAYLAND_DISPLAY  # commented out (do not override DISPLAY globally)
 
-### EXPORTS
-export VISUAL='nvim'
-export EDITOR="$VISUAL"
-export TERMINAL='kitty'
-export BROWSER='zen'
-export PATH="$PATH:/opt/nvim-linux64/bin"
-export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-export TERM_EMULATOR='kitty'
-export TERM='xterm-kitty'
-export GDK_BACKEND=wayland
-export QT_QPA_PLATFORM=wayland
-export XDG_SCREENSHOTS_DIR="$HOME/Pictures/Screenshots"
-export MANPAGER='nvim +Man!'
+### EXPORTS / ENV
+set -gx VISUAL nvim
+set -gx EDITOR $VISUAL
+# TERMINAL is already set above as a universal var
+# set -gx TERMINAL 'kitty'
+set -gx BROWSER zen
+# Prefer fish_add_path instead of editing PATH directly
+if not contains /opt/nvim-linux64/bin $PATH
+    fish_add_path /opt/nvim-linux64/bin
+end
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+set -gx TERM_EMULATOR kitty
+set -gx TERM xterm-kitty
+# set -gx GDK_BACKEND wayland   # commented out above
+# set -gx QT_QPA_PLATFORM wayland  # commented out above
+set -gx XDG_SCREENSHOTS_DIR "$HOME/Pictures/Screenshots"
+set -gx MANPAGER 'nvim +Man!'
 
 ### MY CUSTOM ALIASES
-alias bashedit='nvim ~/.bashrc --allow-root'
-alias fishedit='nvim ~/.config/fish/config.fish'
-alias neofetch_edit='sudo nano ~/.config/neofetch/config.conf'
-alias catnap_edit='nvim ~/.config/catnap/config.toml'
-alias alac_edit='nvim ~/.config/alacritty/alacritty.toml'
+alias bashedit 'nvim ~/.bashrc --allow-root'
+alias fishedit 'nvim ~/.config/fish/config.fish'
+alias neofetch_edit 'sudo nano ~/.config/neofetch/config.conf'
+alias catnap_edit 'nvim ~/.config/catnap/config.toml'
+alias alac_edit 'nvim ~/.config/alacritty/alacritty.toml'
 
-alias mirrors_list='cat /etc/pacman.d/mirrorlist'
-alias mirrors_find='reflector --latest 20 --sort rate --protocol https'
-# alias mirrors_update='sudo reflector --verbose --latest 20 --sort rate --protocol https --timeout 10 --threads 4 --save /etc/pacman.d/mirrorlist'
-alias mirrors_update='sudo reflector --verbose --latest 20 --sort rate --protocol https --threads 4 --save /etc/pacman.d/mirrorlist'
-alias list_gnome_extensions='ls -1 ~/.local/share/gnome-shell/extensions/'
-alias show_opened_ports='lsof -i -P -n | grep LISTEN'
-alias pogoda_minsk='curl -4 https://wttr.in/Minsk'
-alias ventoy_update='sudo ~/soft/ventoy-1.1.05/Ventoy2Disk.sh -u /dev/sda'
-alias refind_conf_edit1='sudo nano /boot/refind_linux.conf'
-alias refind_conf_edit2='sudo nano /boot/EFI/refind/refind.conf'
-alias resolution_back='xrandr -s 2560x1600'
-alias ssid_level='iwconfig wlan0 | gnmcli dev wifi'
-alias change_shell_to_bash='chsh -s /bin/bash'
-alias change_shell_to_fish='chsh -s /usr/bin/fish'
-alias docker_mem_usage='docker stats --no-stream'
-alias established='netstat -anp | grep ESTABLISHED'
+alias mirrors_list 'cat /etc/pacman.d/mirrorlist'
+alias mirrors_find 'reflector --latest 20 --sort rate --protocol https'
+# alias mirrors_update 'sudo reflector --verbose --latest 20 --sort rate --protocol https --timeout 10 --threads 4 --save /etc/pacman.d/mirrorlist'
+alias mirrors_update 'sudo reflector --verbose --latest 20 --sort rate --protocol https --threads 4 --save /etc/pacman.d/mirrorlist'
+alias list_gnome_extensions 'ls -1 ~/.local/share/gnome-shell/extensions/'
+alias show_opened_ports 'lsof -i -P -n | grep LISTEN'
+alias pogoda_minsk 'curl -4 https://wttr.in/Minsk'
+alias ventoy_update 'sudo ~/soft/ventoy-1.1.05/Ventoy2Disk.sh -u /dev/sda'
+alias refind_conf_edit1 'sudo nano /boot/refind_linux.conf'
+alias refind_conf_edit2 'sudo nano /boot/EFI/refind/refind.conf'
+alias resolution_back 'xrandr -s 2560x1600'
+alias ssid_level 'iwconfig wlan0 | gnmcli dev wifi'
+alias change_shell_to_bash 'chsh -s /bin/bash'
+alias change_shell_to_fish 'chsh -s /usr/bin/fish'
+alias docker_mem_usage 'docker stats --no-stream'
+alias established 'netstat -anp | grep ESTABLISHED'
 abbr ipinfo 'curl ipinfo.io'
 abbr ip 'ip -c'
 abbr ipe 'curl ifconfig.co'
 abbr ips 'ip link show'
 abbr wloff 'rfkill block wlan'
 abbr wlon 'rfkill unblock wlan'
-abbr network_restart 'sudo systemctl restart NetworkManager && sudo systemctl restart systemd-resolved && sudo systemctl restart dhcpcd'
+abbr network_restart 'sudo systemctl restart NetworkManager; and sudo systemctl restart systemd-resolved; and sudo systemctl restart dhcpcd'
 abbr -a -g priv 'fish --private'
 abbr -a -g genpass 'openssl rand -base64 10'
 abbr pull_fotos 'adb pull /sdcard/DCIM/Camera/ ~/Downloads/'
@@ -215,8 +218,7 @@ abbr kitty_reload 'killall -USR1 kitty'
 abbr viber_cc 'rm -rf ~/.ViberPC ~/.cache/Viber\ Media\ S.à\ r.l/ViberPC/'
 abbr curl 'curl -#'
 
-# CLI Screenrecorder
-abbr s_r 'gpu-screen-recorder -w screen -f 60 -a $(pactl list sources | grep "Name" | grep -v "monitor" | cut -d " " -f2) -o "$HOME/Videos/Screenrecorder/$(date +%Y-%m-%d-%H%M%S).mp4"'
+abbr s_r 'gpu-screen-recorder -w screen -f 60 -a (pactl list sources | grep "Name" | grep -v "monitor" | cut -d " " -f2) -o "$HOME/Videos/Screenrecorder/(date +%Y-%m-%d-%H%M%S).mp4"'
 abbr rec_sound '$HOME/scripts/screen_record.sh --sound'
 abbr rec_fs '$HOME/scripts/screen_record.sh --fullscreen'
 abbr rec_fs_sound '$HOME/scripts/screen_record.sh --fullscreen-sound'
@@ -244,48 +246,48 @@ function proton_vpn
 end
 
 ### Tools
-alias yt-mp3='cd ~/Downloads && yt-dlp --audio-format mp3 --embed-metadata --audio-quality 0 -x'
+alias yt-mp3 'cd ~/Downloads; and yt-dlp --audio-format mp3 --embed-metadata --audio-quality 0 -x'
 
 ### FUNCTIONS
 
 # function ll
 # clear;
 # tput cup 0 0;
-# ls --color=auto -F --color=always -lhFrt $1;
+# ls --color=auto -F --color=always -lhFrt $argv[1];
 # tput cup 40 0;
 # end
 
 function ex
-    if [ -f $1 ]
-        then
-        switch $1
-            case *.tar.bz2
-                tar xjf $1
-            case *.tar.gz
-                tar xzf $1
-            case *.bz2
-                bunzip2 $1
-            case *.rar
-                unrar x $1
-            case *.gz
-                gunzip $1
-            case *.tar
-                tar xf $1
-            case *.tbz2
-                tar xjf $1
-            case *.tgz
-                tar xzf $1
-            case *.zip
-                unzip $1
-            case *.Z
-                uncompress $1
-            case *.7z
-                7z x $1
+    set -l f $argv[1]
+    if test -f "$f"
+        switch $f
+            case '*.tar.bz2'
+                tar xjf -- "$f"
+            case '*.tar.gz'
+                tar xzf -- "$f"
+            case '*.bz2'
+                bunzip2 -- "$f"
+            case '*.rar'
+                unrar x -- "$f"
+            case '*.gz'
+                gunzip -- "$f"
+            case '*.tar'
+                tar xf -- "$f"
+            case '*.tbz2'
+                tar xjf -- "$f"
+            case '*.tgz'
+                tar xzf -- "$f"
+            case '*.zip'
+                unzip -- "$f"
+            case '*.Z'
+                uncompress -- "$f"
+            case '*.7z'
+                7z x -- "$f"
             case '*'
-                echo "'$1' cannot be extracted via ex()"
+                echo "'$f' cannot be extracted via ex()"
         end
     else
-        echo "'$1' is not a valid file"
+        echo "'$f' is not a valid file"
     end
 end
 
@@ -316,34 +318,34 @@ function backup_nvim
 end
 
 function write_iso
-    # sudo dd if=$1 of=/dev/sda1 bs=4M status=progress conv=fsync
-    sudo dd if=$1 of=/dev/sda1 bs=100M conv=fsync
+    # sudo dd if=$argv[1] of=/dev/sda1 bs=4M status=progress conv=fsync
+    sudo dd if=$argv[1] of=/dev/sda1 bs=100M conv=fsync
 end
 
 # K8Ns
-alias k='kubectl'
-alias m='minikube'
+alias k 'kubectl'
+alias m 'minikube'
 
 # Mining
-alias mining_start='cd ~/soft/NBMiner_Linux && ./start_eth.sh'
-alias edit_miner='nvim ~/soft/NBMiner_Linux/start_eth.sh'
+alias mining_start 'cd ~/soft/NBMiner_Linux; and ./start_eth.sh'
+alias edit_miner 'nvim ~/soft/NBMiner_Linux/start_eth.sh'
 
 # NVIM
 abbr n nvim
-alias v "nvim ."
-alias vim_plugins='nvim ~/.config/nvim/lua/plugins/user.lua'
-alias vim_log='nvim ~/.local/state/nvim/lsp.log'
-alias vim_startify='nvim +:Startify'
-alias vim-astro="NVIM_APPNAME=AstroNvim nvim"
-alias vim-lazy="NVIM_APPNAME=LazyVim nvim"
-abbr vim_minimal nvim -u NONE
+alias v 'nvim .'
+alias vim_plugins 'nvim ~/.config/nvim/lua/plugins/user.lua'
+alias vim_log 'nvim ~/.local/state/nvim/lsp.log'
+alias vim_startify 'nvim +:Startify'
+alias vim-astro "NVIM_APPNAME=AstroNvim nvim"
+alias vim-lazy "NVIM_APPNAME=LazyVim nvim"
+abbr vim_minimal 'nvim -u NONE'
 
 function :q
     exit
 end
 
 ### hack
-alias hack_168='cd /home/jacky/hack && sudo aircrack-ng -w ./dict/rockyou.txt 68:ff:7b:27:96:cf ./168/clean-168.cap'
+alias hack_168 'cd /home/jacky/hack; and sudo aircrack-ng -w ./dict/rockyou.txt 68:ff:7b:27:96:cf ./168/clean-168.cap'
 
 function beep
     # paplay /home/jacky/Music/ringtones/keep-moving_ring.mp3
@@ -352,35 +354,38 @@ end
 
 ### git
 abbr gl 'git log --oneline --graph'
-abbr gs 'clear && git log --graph --stat'
-abbr gss 'clear && git log --stat --color -p'
+abbr gs 'clear; and git log --graph --stat'
+abbr gss 'clear; and git log --stat --color -p'
 
 function gcl
     set directory (echo $argv | grep -oE '[^/]+$' | sed 's/.git//')
-    git clone $argv && cd $directory
+    git clone $argv; and cd $directory
 end
 
 function gcld
     set directory (echo $argv | grep -oE '[^/]+$' | sed 's/.git//')
-    git clone --depth 1 $argv && cd $directory
+    git clone --depth 1 $argv; and cd $directory
 end
 
-### FZF
-fzf --fish | source
+### FZF (guarded)
+if type -q fzf
+    fzf --fish | source
+end
 
-export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
-export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
-export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+set -gx FZF_DEFAULT_COMMAND "fd --hidden --strip-cwd-prefix --exclude .git"
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_ALT_C_COMMAND "fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+set -gx FZF_CTRL_T_OPTS "--preview 'bat -n --color=always --line-range :500 {}'"
+set -gx FZF_ALT_C_OPTS "--preview 'eza --tree --color=always {} | head -200'"
 
 function _fzf_compgen_path
-    fd --hidden --exclude .git . $1
+    fd --hidden --exclude .git . $argv[1]
 end
 
+# Rewritten to fish syntax (no local/shift)
 function _fzf_comprun
-    local command=$1
-    shift
+    set -l command $argv[1]
+    set -e argv[1]
 
     switch $command
         case cd
@@ -388,32 +393,34 @@ function _fzf_comprun
         case ssh
             fzf --preview "eval 'echo \$' {}" $argv
         case '*'
-            fzf --preview "--preview 'bat -n --color=always --line-range :500 {}'" $argv
+            fzf --preview "bat -n --color=always --line-range :500 {}" $argv
     end
 end
 
-alias ls="eza --color=always --long --git --icons=always"
-# alias lll='eza -la --git'
-alias cdi='zi'
+alias ls "eza --color=always --long --git --icons=always"
+# alias lll 'eza -la --git'
+alias cdi 'zi'
 
 function cd
-    # builtin cd $argv
-    z $argv
+    if type -q z
+        z $argv
+    else
+        builtin cd $argv
+    end
     l
 end
 
 function mkcd
-    mkdir -p $argv && cd $argv
+    mkdir -p $argv; and cd $argv
 end
 
 # SQL
 abbr mysql mariadb
 
-# Detect AUR wrapper
-if pacman -Qi yay &>/dev/null
+# Detect AUR wrapper (use type -q, not &>/dev/null)
+if type -q yay
     set aurhelper yay
-end
-if pacman -Qi paru &>/dev/null
+else if type -q paru
     set aurhelper paru
 end
 
@@ -470,7 +477,7 @@ function record_selection_to_gif
 
     cd $HOME/Videos/Screenrecorder/
     set temp_file "./temp_recording.mp4"
-    set output "recording_$(date +%Y-%m-%d_%H.%M.%S).gif"
+    set output "recording_(date +%Y-%m-%d_%H.%M.%S).gif"
 
     if test -e $temp_file
         rm $temp_file
@@ -496,13 +503,13 @@ fish_add_path ~/.volta/bin
 #################
 
 # set -U fish_greeting
-# set -U fish_greeting "Weather update: $(curl -s 'wttr.in?format=%C+%t')"
-set -U fish_greeting "Welcome, $(whoami), to Fish Shell on $(uname -n) running $(uname -o) $(uname -r)"
+# set -U fish_greeting "Weather update: (curl -s 'wttr.in?format=%C+%t')"
+set -U fish_greeting "Welcome, "(whoami)", to Fish Shell on "(uname -n)" running "(uname -o)" "(uname -r)""
 
 fish_vi_key_bindings
 
-# neofetch --colors 3 4 5 6 2 9 &&
-# duf --hide special &&
+# neofetch --colors 3 4 5 6 2 9
+# duf --hide special
 # cowfortune
 # fastfetch
 # catnap -d kali
@@ -511,9 +518,16 @@ fish_vi_key_bindings
 if status is-interactive
     if not set -q __NEOFETCH_STARTED
         set -gx __NEOFETCH_STARTED 1
-        neofetch
+        if type -q neofetch
+            neofetch
+        end
     end
 end
 
-zoxide init fish | source
-# starship init fish | source
+if type -q zoxide
+    zoxide init fish | source
+end
+# if type -q starship
+#     starship init fish | source
+# end
+
