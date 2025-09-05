5336de0c495295a6394bd6a08209cfb51b4ed781
Jack Pts <jackpts1981@gmail.com>
2025-09-05 18:03:03 +0300

    [nvim] apply snacks deps for nerdy (fix for latest ver)

--- .config/nvim/lua/plugins/nerdy.lua
+++ .config/nvim/lua/plugins/nerdy.lua
@@ -2,8 +2,9 @@ return {
   {
     "2kabhishek/nerdy.nvim",
     dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-telescope/telescope.nvim",
+      -- "stevearc/dressing.nvim",
+      -- "nvim-telescope/telescope.nvim",
+        'folke/snacks.nvim',
     },
     cmd = {
       "Nerdy",
