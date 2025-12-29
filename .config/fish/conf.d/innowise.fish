0edbe636cecffa20d839e31cef6c1235a7263591
Jack Pts <jackpts1981@gmail.com>
2025-12-29 13:02:33 +0300

    [fish][inno] projects abbrs added

--- .config/fish/conf.d/innowise.fish
+++ .config/fish/conf.d/innowise.fish
@@ -1,10 +1,14 @@
 # SL Project
 abbr sl_vpn_on 'sudo wg-quick up /etc/wireguard/clients/slm-yp.conf'
 abbr sl_vpn_off 'sudo wg-quick down /etc/wireguard/clients/slm-yp.conf'
+abbr sl_be_start 'cd $HOME/bitbucket/SL/sl_back && npm run start:dev'
+abbr sl_fe_start 'cd $HOME/bitbucket/SL/sl_frontend && npm run dev'
+abbr sl_db_check 'psql -h localhost -p 5432 -U root -d develop -c "\conninfo"'
 
 # BitBucket check via SSH
 abbr bb_test 'ssh -T git@bitbucket.org'
 
 # Google Chat Bot Project
 abbr gcb_tun_up 'cd $HOME/gitlab/googlechatbot && lt --port 3000'
-
+abbr gcb_url 'lt --port 3000'
+abbr gcb_start 'cd $HOME/gitlab/googlechatbot && npm run dev'
