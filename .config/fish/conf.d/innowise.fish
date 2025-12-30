15f18949e130326aa26a61e488fc814297ef133a
Jack Pts <jackpts1981@gmail.com>
2025-12-30 11:25:41 +0300

    [fish][inno] add project backup method

--- .config/fish/conf.d/innowise.fish
+++ .config/fish/conf.d/innowise.fish
@@ -5,6 +5,18 @@ abbr sl_be_start 'cd $HOME/bitbucket/SL/sl_back && npm run start:dev'
 abbr sl_fe_start 'cd $HOME/bitbucket/SL/sl_frontend && npm run dev'
 abbr sl_db_check 'psql -h localhost -p 5432 -U root -d develop -c "\conninfo"'
 
+function sl_backup --description 'Create SL project archive excluding node_modules and *.dump files'
+    set project_dir "$HOME/bitbucket/SL"
+    set backup_dir "$HOME/backup"
+    set timestamp (date "+%Y%m%d_%H%M%S")
+    set archive_name "sl_$timestamp.7z"
+    set archive_path "$backup_dir/$archive_name"
+
+    mkdir -p $backup_dir
+    7z a -xr'!node_modules' -xr'!*.dump' -xr'!dist' "$archive_path" "$project_dir/"
+end
+abbr sl_backup 'sl_backup'
+
 # BitBucket check via SSH
 abbr bb_test 'ssh -T git@bitbucket.org'
 
